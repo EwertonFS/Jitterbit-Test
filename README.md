894dc45612bfb7b2fd2480ded178d603cb6651ce
ewertonfs <ewerton.lost2@gmail.com>
2026-03-09 13:55:10 -0300

    chore: Initial project setup

--- README.md
+++
@@ -1,1 +0,0 @@
-# Jitterbit-Test
---
+++ src/lib/prisma.ts
@@ -0,0 +1,13 @@
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../database/client';
+
+const connectionString = process.env.DATABASE_URL;
+
+function createPrismaClient() {
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    return new PrismaClient({ adapter });
+}
+
+export const prisma = createPrismaClient();
